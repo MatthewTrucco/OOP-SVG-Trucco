86ac7f43fa2d5dfd90f4f4095871b5b16a1752e8
MatthewTrucco <87626582+MatthewTrucco@users.noreply.github.com>
2023-09-08 09:59:53 -0400

    -

--- index.js
+++ index.js
@@ -38,10 +38,44 @@ function writeToFile(fileName, answers) {
 function promptUser() {
     inquirer
       .prompt([
-        // Text prompt
+        //Text Prompt
         {
-          type: "input",
-          message:
-            "What text would you like you logo to display? (Enter up to three characters)",
-          name: "text",
-        },
+            type: "input",
+            message:
+              "What text would you like you logo to display? (Enter up to three characters)",
+            name: "text",
+          },
+          //Text Color
+          {
+            type: "input",
+            message:
+              "Choose text color (Enter color keyword OR a hexadecimal number)",
+            name: "textColor",
+          },
+          //Shape Choice
+          {
+            type: "list",
+            message: "What shape would you like the logo to render?",
+            choices: ["Triangle", "Square", "Circle"],
+            name: "shape",
+          },
+          //Color Choice
+          {
+            type: "input",
+            message:
+              "Choose shapes color (Enter color keyword OR a hexadecimal number)",
+            name: "shapeBackgroundColor",
+          },
+        ])
+        .then((answers) => {
+            if (answers.text.length > 3){
+                console.log("Must enter a value of no more than 3 characters");
+                promptUser()
+            } else {
+
+                writeToFile("logo.svg", answers);
+            }
+        });
+    }
+
+    promptUser();
